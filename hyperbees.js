f6f3b103ee24cfe071eda55b3b8c8d03fb3b6c22
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-07-14 16:01:03 +0200

    prozedur um CPU aud Muon auzulesen

---
+++ hyperbees.js
@@ -0,0 +1,52 @@
+const chalk = require('chalk')
+const Hypercore = require('hypercore')
+const ram = require('random-access-memory')
+const Hyperbee = require('hyperbee')
+
+start()
+
+async function start () {
+  // A Hyperbee is stored as an embedded index within a single Hypercore.
+  const core = new Hypercore(ram)
+
+  // It accepts LevelDB-style key/value encoding options.
+  const db = new Hyperbee(core, {
+    keyEncoding: 'utf-8',
+    valueEncoding: 'utf-8'
+  })
+  await db.ready()
+
+
+  
+/**Beispiel */
+  // Key/value pairs can be inserted with the `put` method.
+  await db.put('a', 'b')
+  await db.put('c', 'd')
+
+  // You can do large, bulk insertions with the `batch` method.
+  // A Batch object mirrors the Hyperbee API.
+  const b = db.batch()
+  await b.put('e', 'f')
+  await b.put('g', 'h')
+
+  // When a batch is flushed, it's atomically committed to the Hyperbee.
+  await b.flush()
+
+  // KV-pairs can be deleted with the `del` method.
+  await db.del('c')
+
+  console.log(chalk.green('Reading KV-pairs with the \'get\' method:\n'))
+
+  // KV-pairs can be read with the `get` method.
+  console.log('Value for \'a\':', (await db.get('a')).value)
+  console.log('Value for \'e\':', (await db.get('e')).value)
+  console.log('Value for \'c\' (deleted):', await db.get('c'))
+
+  console.log(chalk.green('\nReading KV-pairs with \'createReadStream\':\n'))
+
+  // createReadStream can be used to yield KV-pairs in sorted order.
+  // createReadStream returns a ReadableStream that supports async iteration.
+  for await (const { key, value } of db.createReadStream()) {
+    console.log(`${key} -> ${value}`)
+  }
+}
---
+++ readMuonCPU.js
@@ -0,0 +1,11 @@
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec)
+
+async function getCPUtemperature () {
+  // Exec output contains both stderr and stdout outputs
+  const temperatureOutput = await exec('echo "Date $(date +"%d.%m.%y"), Time $(date +"%T"), CPU_temp: $((cpu/1000))')
+  console.log(temperatureOutput)
+
+};
+
+getCPUtemperature()
