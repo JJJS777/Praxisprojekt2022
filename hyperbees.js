bc184e452033190fdfe9a0e125dd18210cf820f2
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-07-18 18:13:15 +0200

    ...testen ...

--- hyperbees.js
+++ hyperbees.js
@@ -4,13 +4,24 @@ const ram = require('random-access-memory')
 const Hyperbee = require('hyperbee')
 const readCPU = require('./readMuonCPU');
 const beeLoggo = 'LOGGO FROM BEE-CORE: '
+const Hyperswarm = require('hyperswarm')
 
+
+/**IMPEMENTIERUNG DER NODES: Sensor-Server-Node-1 und 2 
+ * 
+ * 
+ * 
+*/
 
 start()
 
 async function start() {
   // A Hyperbee is stored as an embedded index within a single Hypercore.
   const core = new Hypercore(ram)
+  const swarmServer = new Hyperswarm()
+
+  await core.ready()
+  console.log( beeLoggo + " KEY: " + core.keyclear + " KEY-Pair: " + core.keyPair + " discoveryKey: " + core.discoveryKey)
 
   // It accepts LevelDB-style key/value encoding options.
   const db = new Hyperbee(core, {
@@ -20,18 +31,31 @@ async function start() {
   await db.ready()
 
 
+  swarmServer.on('connection', (conn, peerInfo) => {
+    // swarm1 will receive server connections
+    conn.write('this is a server connection')
+    console.log(beeLoggo + peerInfo.publicKey + peerInfo.topics)
+    conn.on('data', data => console.log('server got message:', data.toString()))
+    conn.end()
+  })
+
+  const topic = Buffer.alloc(32).fill('sensor data') // A topic must be 32 bytes
+  const discovery = swarmServer.join(topic, { server: true, client: false })
+  await discovery.flushed() // Waits for the topic to be fully announced on the DHT
+
+  // After this point, both client and server should have connections
+
   for (var i = 0; i < 5; i++) {
 
     const returnValues = await readCPU()
     // dateTime = returnValues.date
     // temprature = returnValues.temp
     await db.put(returnValues.date, returnValues.temp)
-    console.log( beeLoggo + "PUT Date: " + returnValues.date + " and " + returnValues.temp)
+    console.log(beeLoggo + "PUT Date: " + returnValues.date + " and " + returnValues.temp)
     // After the append, we can see that the length has updated.
     console.log(beeLoggo + 'Length of the first core:', core.length) // Will be 2.
     await sleep(5000)
   }
-
   console.log(chalk.green(beeLoggo + 'Reading KV-pairs with the \'get\' method:\n'))
 
   // createReadStream can be used to yield KV-pairs in sorted order.
--- hypercore.js
+++ hypercore.js
@@ -2,8 +2,17 @@ const chalk = require('chalk')
 const Hypercore = require('hypercore')
 const ram = require('random-access-memory')
 const soloCoreLoggo = 'LOGGO FROM SOLO-CORE-APP: '
+const Hyperswarm = require('hyperswarm')
 
-async function core() {
+
+/**IMPEMENTIERUNG DER NODES: Nodes 3- x 
+ * 
+ * 
+*/
+
+coreX()
+
+async function coreX() {
 
   // Step 1: Create our initial Hypercore.
   console.log(chalk.green(soloCoreLoggo + 'Step 1: Create the initial Hypercore\n'))
@@ -12,20 +21,29 @@ async function core() {
   try {
     /**Creating Hypercore Instance */
     const core = new Hypercore('./my-hypercore', this.key, { createIfMissing: true, valueEncoding: 'json' })
+    const swarmClient = new Hyperswarm()
 
-    // Append two new blocks to the core.
-    await core.append(['hello', 'world from 777'])
+    console.log(soloCoreLoggo + core.key + core.keyPair)
+
+
+    swarmClient.on('connection', (conn, peerInfo) => {
+      conn.on('data', data => console.log('client got message:', data.toString()))
+      console.log(soloCoreLoggo + peerInfo.publicKey + peerInfo.topics)
+      conn.write('hello from client-node3, can is send queries over this chennel?')
+    })
+
+    const topic = Buffer.alloc(32).fill('sensor data') // A topic must be 32 bytes   
+    swarmClient.join(topic, { server: false, client: true })
+    await swarmClient.flush() // Waits for the swarm to connect to pending peers.
+
+    // After this point, both client and server should have connections
 
     // After the append, we can see that the length has updated.
-    console.log( soloCoreLoggo + 'Length of the first core:', core.length) // Will be 2.
+    console.log(soloCoreLoggo + 'Length of the first core:', core.length) // Will be 2.
 
     await core.close()
-    return core
 
   } catch (error) {
     console.error('Error in creating Hypercore', error)
   }
-
 }
-
-module.exports = core
