284971c71259390f4ca3287f7f7157af00370e28
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-07-21 19:07:48 +0200

    fehler....fml

--- src/node-3.js
+++ src/node-3.js
@@ -1,2 +1,34 @@
 const Hypercore = require('hypercore')
+const Hyperswarm = require('hyperswarm')
 
+
+
+
+node3()
+
+async function node3() {
+    var remotePublicKey = null
+    const swarm = new Hyperswarm()
+
+    swarm.on('connection', (socket, peerInfo) => {
+        socket.on('data', data => {
+            remotePublicKey = data
+            console.log('Remote-public-key: ' + remotePublicKey.key.toString('hex'))
+        })
+    })
+
+    const topic = Buffer.alloc(32).fill('test') // A topic must be 32 bytes
+    swarm.join(topic, { server: false, client: true })
+    await swarm.flush() // Waits for the swarm to connect to pending peers.
+    // After this point, both client and server should have connections
+
+
+    const core = new Hypercore('./node3', remotePublicKey, {
+        valueEncoding: 'utf-8',
+        sparse: true, // When replicating, don't eagerly download all blocks.
+    })
+    await core.ready()
+
+    console.log('Local-public-key: ' + core.key.toString('hex'))
+
+}
--- src/sensor-Server-Node-1.js
+++ src/sensor-Server-Node-1.js
@@ -1,4 +1,26 @@
 const chalk = require('chalk')
 const Hypercore = require('hypercore')
+const Hyperswarm = require('hyperswarm')
 
+sensorServerNode1()
 
+async function sensorServerNode1() {
+    const swarm = new Hyperswarm()
+    const core = new Hypercore('./sensor-Server-Node-1')
+    await core.ready()
+
+    console.log('Server-Public-Key: ' + core.key.toString('hex'))
+
+    // Append two new blocks to the core.
+    await core.append(['hello', 'world', 'from', 'server'])
+
+    // After the append, we can see that the length has updated.
+    console.log('Length of the first core:', core.length)
+
+    swarm.on('connection', (socket, peerInfo) => {
+        socket.write(core.key)
+    })
+    const topic = Buffer.alloc(32).fill('test') // A topic must be 32 bytes
+    const discovery = swarm.join(topic, { server: true, client: false })
+    await discovery.flushed() // Waits for the topic to be fully announced on the DHT
+}
