a413718dea9aa1c8eabb8d4c8f1ebd6517610a6f
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-07-25 12:02:33 +0200

    hyperbee hinzugefügt, alles läuft

--- src/node-3.js
+++ src/node-3.js
@@ -4,25 +4,37 @@ const Hyperswarm = require('hyperswarm')
 const Hypercore = require('hypercore')
 const Hyperbee = require('hyperbee')
 const chalk = require('chalk')
-const KEY = 'b6bfef1450c8467926efc3e22b8376504d2433f87ea60b02dab76ed43e4615d9'// Insert the key served by your server here (as string)
+const KEY = '6c51268c8194b05f7f8cbb3cab4869726033d7997a38edadb86f40f63b82fa39'// Insert the key served by your server here (as string)
 
 node3()
 
-async function node3(){
+async function node3() {
   const core = new Hypercore('./node-3', Buffer.from(KEY, "hex"))
+  const bee = new Hyperbee(core, {keyEncoding: "utf-8", valueEncoding: "utf-8"} )
   await core.ready()
 
   const swarm = new Hyperswarm()
   swarm.on('connection', socket => core.replicate(socket))
   swarm.join(core.discoveryKey, { server: false, client: true })
-  
+
   console.log(chalk.green('Local-Public-Key: '), core.key.toString('hex'))
   await swarm.flush()
 
-    // Now we can read blocks from the core.
-  // Note that these blocks will be downloaded lazily, when each one requested.
-  console.log('First core block:', await core.get(2)) // 'hello'
-  console.log('Second core block:', await core.get(3)) // 'world'
+
+  // Ensure we are connected to at least 1 peer (else getting simply returns null)
+  if (bee.feed.writable || bee.feed.peers.length) {
+    console.log("Writable or already found peers for core");
+  } else {
+    console.log("Waiting for peers to connect");
+    const [peer] = await once(bee.feed, "peer-add");
+    console.log("Connected to peer", peer.remotePublicKey);
+  }
+  console.log((await bee.get(2)))
+
+  const readStream = await bee.createReadStream()
+  for await (const entry of readStream) {
+    console.log(entry)
+  }
 
   console.log("finished")
 }
