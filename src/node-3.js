7dce84c13adad1b735c6cfd809323ad9e3582aa0
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-07-25 10:25:13 +0200

    Hypercore + Hyperswarm implementiert. Node3 kann daten aus Remote-Core lesen

--- src/node-3.js
+++ src/node-3.js
@@ -1,22 +1,27 @@
-const Hypercore = require('hypercore')
-const Hyperswarm = require('hyperswarm')
-const chalk = require('chalk')
+// Client
 
+const Hyperswarm = require('hyperswarm')
+const Hypercore = require("hypercore")
+const chalk = require('chalk')
+const KEY = '84478400b1ccfd27bbb5d0bb627920737cf107f53d7333031c08b60a9ea6124a'// Insert the key served by your server here (as string)
 
 node3()
 
-async function node3() {
-    var remotePublicKey = '6d358152d1964f725b19e406c9a2a0e22b207b4861ca2c50c13649320f9bfb8a'
-    const core = new Hypercore('./node-3', Buffer.from(remotePublicKey, 'hex'))
-    core.ready
-    console.log(chalk.green('Local-public-key: ') + core.key.toString('hex'))
+async function node3(){
+  const core = new Hypercore('./node-3', Buffer.from(KEY, "hex"))
+  await core.ready()
 
-    const swarm = new Hyperswarm()
-    swarm.on('connection', (socket, peerInfo) => {
-        core.replicate(socket)
-    })
+  const swarm = new Hyperswarm()
+  swarm.on('connection', socket => core.replicate(socket))
+  swarm.join(core.discoveryKey, { server: false, client: true })
+  
+  console.log(chalk.green('Local-Public-Key: '), core.key.toString('hex'))
+  await swarm.flush()
 
-    swarm.join(core.discoveryKey, { server: false, client: true })
-    console.log('Requesting core:', core.key.toString('hex'))
-    await swarm.flush()
+    // Now we can read blocks from the core.
+  // Note that these blocks will be downloaded lazily, when each one requested.
+  console.log('First core block:', await core.get(2)) // 'hello'
+  console.log('Second core block:', await core.get(3)) // 'world'
+
+  console.log("finished")
 }
