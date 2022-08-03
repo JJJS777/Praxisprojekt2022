18861ba35b2d46ea6bcad5b7fb3316eff925452a
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-08-03 18:52:52 +0200

    methode queryRemoteNode hinzugefügt

--- src/sensor-Server-Node.js
+++ src/sensor-Server-Node.js
@@ -4,7 +4,7 @@ const Hypercore = require('hypercore')
 const Hyperswarm = require('hyperswarm')
 const Hyperbee = require('hyperbee')
 const Corestore = require('corestore')
-const SHARED_PUBLIC_KEY = '6c51268c8194b05f7f8cbb3cab4869726033d7997a38edadb86f40f63b82fa39'
+const PUBLIC_KEY_SENSOR_NODE = '6c51268c8194b05f7f8cbb3cab4869726033d7997a38edadb86f40f63b82fa39'
 //const core = new Hypercore('./sensor-Server-Node-1', Buffer.from(SHARED_PUBLIC_KEY, "hex"))
 
 
@@ -43,20 +43,22 @@ async function sensorNode(nodeNumber) {
 
   //**Connect to DHT */
   swarm.on('connection', (socket, peerInfo) => {
-    core.replicate(socket)
+    localCore.replicate(socket)
   })
-  swarm.join(core.discoveryKey, { server: true, client: false })
+  swarm.join(localCore.discoveryKey, { server: true, client: false })
   swarm.flush()
+
+  await queryRemoteNode(localStore, swarm)
 
 }
 
+
+//**Helper Funktions */
 async function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
   });
 }
-
-
 
 async function initHyperbee(core) {
   const bee = new Hyperbee(core, { keyEncoding: "utf-8", valueEncoding: "utf-8" })
@@ -64,4 +66,37 @@ async function initHyperbee(core) {
   return bee
 }
 
-async function queryNode() { }
+async function queryRemoteNode(localStore, swarm) {
+  //**KLÄREN: Neuen Hyperswarm init oder kann man den bereits init swarm nutzen? */
+
+  /**Init Remote Hypercore */
+  const remoteCore = localStore.get(Buffer.from(PUBLIC_KEY_SENSOR_NODE, "hex"))
+  remoteCore.ready()
+
+  //**Load Data from Remote Hyperbee */
+  const remoteBee = await initHyperbee(remoteCore)
+
+  //**Connect to DHT */
+  swarm.on('connection', (socket, peerInfo) => {
+    remoteCore.replicate(socket)
+  })
+  swarm.join(remoteCore.discoveryKey, { server: false, client: true })
+  swarm.flush()
+
+  // Ensure we are connected to at least 1 peer (else getting simply returns null)
+  if (remoteBee.feed.writable || remoteBee.feed.peers.length) {
+    console.log("Writable or already found peers for core");
+  } else {
+    console.log("Waiting for peers to connect");
+    const [peer] = await once(remoteBee.feed, "peer-add");
+    console.log("Connected to peer", peer.remotePublicKey);
+  }
+  console.log((await bee.get(2)))
+
+  const readStream = await remoteBee.createReadStream()
+  for await (const entry of readStream) {
+    console.log(entry)
+  }
+
+  console.log("finished")
+}
