09e2920a66b3d9dbffe33f192cffbe80c1bf97a7
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-09-22 16:52:42 +0200

    methode zum auslesen der cpu angepasst

--- code/src/helper/readCPU.js
+++ code/src/helper/readCPU.js
@@ -1,12 +1,13 @@
 const { promisify } = require('util');
 const exec = promisify(require('child_process').exec)
 
-// echo "CPU_temp: $((cpu/1000))'C" */
+/**Der Befehle geht sicher: cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
+echo "CPU_temp: $((cpu/1000))" */
 
 async function getCPUtemperature() {
   // Exec output contains both stderr and stdout outputs
   const dateTimeOutput = await exec('echo "Date $(date +"%d.%m.%y"), Time $(date +"%T")"')
-  const temperatureOutput = await exec('echo "CPU_temp: $((cpu/1000))"')
+  const temperatureOutput = await exec('cpu=$(cat /sys/class/thermal/thermal_zone0/temp) echo "CPU_temp: $((cpu))"')
 
   return {
     date: dateTimeOutput.stdout.trim(),
