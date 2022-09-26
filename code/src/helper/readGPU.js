c5c6f3939302db3de0aba419a77fc75851542565
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-09-26 11:15:55 +0200

    schleife in readGPU

--- code/src/helper/readGPU.js
+++ code/src/helper/readGPU.js
@@ -4,16 +4,30 @@ const exec = promisify(require('child_process').exec)
 /**Der Befehle geht sicher: cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 echo "GPU_$(/opt/vc/bin/vcgencmd measure_temp), CPU_temp: $((cpu/1000))'C" */
 
-async function getGPUtemperature() {
+async function getGPUtemperature(core, hyperbee) {
+
+  const bee = hyperbee
+  const localCore = core
+
   // Exec output contains both stderr and stdout outputs
   const dateTimeOutput = await exec('echo "Date $(date +"%d.%m.%y"), Time $(date +"%T")"')
   const temperatureOutput = await exec('echo "GPU_$(/opt/vc/bin/vcgencmd measure_temp)"')
 
-  return {
-    date: dateTimeOutput.stdout.trim(),
-    temp: temperatureOutput.stdout.trim()
+  while (true) {
+    await bee.put(dateTimeOutput, temperatureOutput)
+    console.log("PUT Date: " + dateTimeOutput + " and " + temperatureOutput)
+    // After the append, we can see that the length has updated.
+    console.log('Length of the first core:', localCore.length)
+    await sleep(20000)
   }
 };
+
+//**Helper Funktions */
+async function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
 
 module.exports = getGPUtemperature
 
