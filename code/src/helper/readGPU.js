a5c6f11c4188e85b0b1d4b06ce63b97028fdffb0
Julian Schiller <julian.schiller@smail.th-koeln.de>
2022-10-03 11:42:27 +0200

    Revert "schleife in readGPU"
    This reverts commit c5c6f3939302db3de0aba419a77fc75851542565.

--- code/src/helper/readGPU.js
+++ code/src/helper/readGPU.js
@@ -4,30 +4,16 @@ const exec = promisify(require('child_process').exec)
 /**Der Befehle geht sicher: cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
 echo "GPU_$(/opt/vc/bin/vcgencmd measure_temp), CPU_temp: $((cpu/1000))'C" */
 
-async function getGPUtemperature(core, hyperbee) {
-
-  const bee = hyperbee
-  const localCore = core
-
+async function getGPUtemperature() {
   // Exec output contains both stderr and stdout outputs
   const dateTimeOutput = await exec('echo "Date $(date +"%d.%m.%y"), Time $(date +"%T")"')
   const temperatureOutput = await exec('echo "GPU_$(/opt/vc/bin/vcgencmd measure_temp)"')
 
-  while (true) {
-    await bee.put(dateTimeOutput, temperatureOutput)
-    console.log("PUT Date: " + dateTimeOutput + " and " + temperatureOutput)
-    // After the append, we can see that the length has updated.
-    console.log('Length of the first core:', localCore.length)
-    await sleep(20000)
+  return {
+    date: dateTimeOutput.stdout.trim(),
+    temp: temperatureOutput.stdout.trim()
   }
 };
-
-//**Helper Funktions */
-async function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
-}
 
 module.exports = getGPUtemperature
 
